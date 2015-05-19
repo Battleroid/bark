c0dbc5259b9f53f406cdedc348db2e18fd00a44f
Battleroid <casweed@gmail.com>
2015-05-18 22:59:04 -0400

    update gitignore, add bark

---
+++ bark.py
@@ -0,0 +1,23 @@
+import frontmatter
+import misaka
+import jinja2
+import yaml
+
+__doc__ = """Basic little static site generator, nothing fancy. Just tired of all the other junk right now that requires
+learning billions and billions of new ways and configurations to literally just take markdown and convert it to html."""
+
+def parse_config(f='config.yaml'):
+    pass
+
+def render_post(f):
+    pass
+
+def generate_index():
+    pass
+
+def new_site(name):
+    pass
+
+if __name__ == '__main__':
+    # parse cmd line args
+    pass
